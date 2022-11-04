33d98872dacc5af8c1f5ac29d91a2231540a8f73
Rbrandb <87430190+Rbrandb@users.noreply.github.com>
2022-11-04 12:06:12 -0400

    Update

--- xf_partner_contract/static/src/js/side_bar.js
+++ xf_partner_contract/static/src/js/side_bar.js
@@ -2,17 +2,11 @@
 
     import ActionMenus from "web.ActionMenus";
     import { patch } from 'web.utils';
-
-//    console.log("ooooooo",ActionMenu);
-//    const Context = require('web.Context');
-//    const DropdownMenu = require('web.DropdownMenu');
     patch(ActionMenus.prototype, 'xf_partner_contract/static/src/js/side_bar.js', {
         async _setPrintItems(props) {
-            if (this.env.view.base_model === "account.move") {
+            if (this.env.view.base_model === "account.move" || 'xf.partner.contract') {
                 props.items.print = [];
             }
             return this._super(...arguments)
         }
     })
-
-//});
